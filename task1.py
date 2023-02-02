f868ce77b79769d2b857c140601675746232cdfd
mamonchikmarina <mamonchik.marina@yandex.ru>
2023-02-02 22:34:46 +0300

    task1_solution

--- task1.py
+++ task1.py
@@ -5,3 +5,11 @@
 123 -> 6 (1 + 2 + 3)
 100 -> 1 (1 + 0 + 0)
 """
+print ('введите трехзначное число: ')
+n = int(input ())
+summa = 0
+while n > 0:
+    x = n % 10
+    summa = summa + x
+    n = n // 10
+print (summa)
